38f305fd31168b43f75cb59bd7a1b8ad3233bdbd
sariyadilak <ariyadilak.s@gmail.com>
2018-02-08 10:39:06 -0500

    track location and earthquake

--- ucesriy/www/js/appActivity.js
+++ ucesriy/www/js/appActivity.js
@@ -1,3 +1,36 @@
+function loadEarthquakeData(){
+					// call the getEarthquakes code
+					// keep the alert message so that we know something is happening
+					alert("Loading Earthquakes");
+					getEarthquakes();
+			}
+			// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable
+			var client;
+			// create the code to get the Earthquakes data using an XMLHttpRequest
+			function getEarthquakes(){
+				client = new XMLHttpRequest();
+			client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+				client.onreadystatechange = earthquakeResponse;
+				client.send();
+			}
+			// create the code to wait for the response from the data server, and process the response once it is received
+			function earthquakeResponse(){
+				// this function listens out for the server to say that the data is ready - i.e. has state 4
+				if (client.readyState == 4) {
+					//once the data is ready, process the data
+					var earthquakedata = client.responseText;
+					loadEarthquakelayer(earthquakedata);
+					}
+			}
+			// convert the received data - which is text - to JSON format and add it to the map
+			function loadEarthquakelayer(earthquakedata){
+				// convert the text to JSON
+				var earthquakejson = JSON.parse(earthquakedata);
+				// add the JSON layer onto the map - it will appear using the default icons
+				earthquakelayer = L.geoJson(earthquakejson).addTo(mymap);
+				// change the map zoom so that all the data is shown
+				mymap.fitBounds(earthquakelayer.getBounds());
+			}
 function trackLocation() {
 if (navigator.geolocation) {
 	navigator.geolocation.watchPosition(showPosition);
@@ -6,11 +39,8 @@ if (navigator.geolocation) {
 	}
 }
 function showPosition(position) {
-	// add a point
-	L.marker(position.coords.latitude,position.coords.longitude).addTo(mymap).bindPopup("<b>Hello world!</b><br />I am here.").openPopup();
+	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
 }
-
-
 		// load the map
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 		// load the tiles
