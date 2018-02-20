df07672edd435775c94b40372a2fb663746b8ca1
sariyadilak <ariyadilak.s@gmail.com>
2018-02-19 19:09:47 -0500

    add busstop2

--- ucesriy/www/js/appActivity.js
+++ ucesriy/www/js/appActivity.js
@@ -89,41 +89,6 @@ var myPolygon = L.polygon([[51.509, -0.08],[51.503, -0.06],[51.51, -0.047]],{col
 	mymap.fitBounds(busstoplayer.getBounds());
 	}
 
-		// load the map
-		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-		// load the tiles
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {maxZoom: 18,attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' +'Imagery © <a href="http://mapbox.com">Mapbox</a>',id: 'mapbox.streets'}).addTo(mymap);
-		var testMarkerRed = L.AwesomeMarkers.icon({
-			icon:'play',
-			markerColor:'red'
-		});
-		var testMarkerPink = L.AwesomeMarkers.icon({
-			icon:'play',
-			markerColor:'pink'
-		});
-		//customise the loadEarthquakelayer method in the header to use custom icons
-		function loadEarthquakelayer(earthquakedata){
-			//convert the text received from the server to JSON
-			var earthquakejson = JSON.parse(earthquakedata);
-			// load the geoJSON layer
-			var earthquakelayer = L.geoJson(earthquakejson,
-				{
-				// use point to layer to create the points
-				pointToLayer:function(feature,latlng)
-				{
-				// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude use a different marker depending on this value
-				// also include a pop-up that shows the place value of the earthquakes
-				if (feature.properties.mag > 1.75) {
-					return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place+"</b>");
-				}
-				else {
-				// magnitude is 1.75 or less
-				return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place+"</b>");;
-				}
-				},
-				}).addTo(mymap);
-			mymap.fitBounds(earthquakelayer.getBounds());
-			}
 function trackLocation() {
 if (navigator.geolocation) {
 	navigator.geolocation.watchPosition(showPosition);
